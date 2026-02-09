8bcbc6dadd1d0957b10796b7d7b504a5517e7b5a
MatiasCardoso-000 <matias.cardoso3001@gmail.com>
2026-02-09 16:24:41 -0300

    option 2 now show the tasks on the list. option 3 allow to mark complete or not a task.

--- main.py
+++ main.py
@@ -12,19 +12,24 @@ def main():
         inp = input("\nSeleccione una opción: ")
 
         if (inp == "1"):
-            title = input("Título: ")
+            title = input("Ingrese el nombre de la tarea: ")
             taskExist = [t for t in tasks if title == t["title"]]
+
             if taskExist:
                 print("❌❌❌ YA EXISTE LA TAREA PA")
                 menu()
             else:
                 add_tasks(tasks, title)
                 save_task(tasks)
+                print(
+                    f"\n✅ La tarea ha sido agregada a la lista correctamente con el nombre {tasks[len(tasks)-1:][0]["title"]}")
         elif (inp == "2"):
-            print("Ha seleccionado la opción 2\n")
+            print(" \nLISTA DE TAREAS:\n")
             show_tasks(tasks)
         elif (inp == "3"):
             print("Ha seleccionado la opción 3")
+            complete_task(tasks)
+            save_task(tasks)
         elif (inp == "4"):
             print("Ha seleccionado la opción 4")
         else:
--- task_manager.py
+++ task_manager.py
@@ -21,16 +21,31 @@ def add_tasks(tasks, title):
 def save_task(tasks):
     with open(FILE, "w", encoding="UTF-8") as f:
         json.dump(tasks, f, indent=4)
-    print("✅ La tarea ha sido agregada a la lista correctamente.")
+
+   
 
 
 def show_tasks(tasks):
-
-    checkIsDone = list(filter(lambda t: t["done"] == True, tasks))
-
-    for i in range(len(checkIsDone)):
-         if (tasks[i]["id"] == checkIsDone[i]["id"]):
+    for i in range(len(tasks)):
+        if (tasks[i]["done"] is True):
             isDone = "✅"
-         elif (tasks[i]["id"] != checkIsDone[i]["id"]):
+        else:
             isDone = "❌"
-    print(f"LISTA DE TAREAS:\n\n{tasks[i]["id"]}. Tarea: {tasks[i]["title"]} | Finalizada: {isDone}")
+        print(
+            f"{tasks[i]["id"]}. Tarea: {tasks[i]["title"]} | Finalizada: {isDone}")
+
+
+def complete_task(tasks):
+    inpTitle = input("Ingrese el nombre de la tarea a completar: ")
+
+    for t in tasks:
+        if (inpTitle.lower().strip() == t["title"].lower()):
+            inpDone = input("Desea finalizar la tarea? Ingrese si o no\n\n")
+            if (inpDone == "si"):
+                t["done"] = True
+                print("\nLa tarea esta finalizada")
+            elif (inpDone == "no"):
+                t["done"] = False
+                print("\nLa tarea sigue pendiente")
+            else:
+                print("Ingrese una opcion valida")
